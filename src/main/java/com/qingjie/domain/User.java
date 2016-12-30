13173770a267e1a81c0c5eb454266dcb303d6e13
Qingjie Zhao <qzhao02@syr.edu>
2016-12-30 00:58:21 -0500

    test Redis中存储对象

--- src/main/java/com/qingjie/domain/User.java
+++ src/main/java/com/qingjie/domain/User.java
@@ -1,12 +1,19 @@
 package com.qingjie.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6407405456658514269L;
 
 	@Id
 	@GeneratedValue
