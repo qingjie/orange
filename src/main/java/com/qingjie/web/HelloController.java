0d765b11e102fa8253d502811b6fa96399e7bfa1
Qingjie Zhao <qzhao02@syr.edu>
2016-12-30 11:07:51 -0500

    Spring Boot中Web应用的统一异常处理
    http://localhost:8080/json

--- src/main/java/com/qingjie/web/HelloController.java
+++ src/main/java/com/qingjie/web/HelloController.java
@@ -1,24 +1,39 @@
 package com.qingjie.web;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.qingjie.domain.User;
+import com.qingjie.exception.MyException;
 
+/*
 @RestController
 public class HelloController {
 	
 	  @RequestMapping("/hello") 
 	  public String index() { return "Hello World"; }
 	 
+
+}
+*/
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello() throws Exception {
+		throw new Exception("发生错误");
+	}
+
+	@RequestMapping("/json")
+	public String json() throws MyException {
+		throw new MyException("发生错误2");
+	}
+
+	@RequestMapping("/")
+	public String index(ModelMap map) {
+		map.addAttribute("host", "http://www.syr.edu");
+		return "index";
+	}
 
 }
