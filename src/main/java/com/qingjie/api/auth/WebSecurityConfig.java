c5a8d68913451d1b9b049a9265809938780ae4a7
qingjie <qzhao02@syr.edu>
2017-04-22 13:10:58 -0400

    allow swagger in sping-security

--- src/main/java/com/qingjie/api/auth/WebSecurityConfig.java
+++ src/main/java/com/qingjie/api/auth/WebSecurityConfig.java
@@ -43,36 +43,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                // we don't need CSRF because our token is invulnerable
-                .csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                // don't create session
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests()
-                // Allow access public resource
-                .antMatchers(
-                        HttpMethod.GET,
-                        "/",
-                        "/upload/**",
-                        "/*.html",
-                        "/favicon.ico",
-                        "/**/*.html",
-                        "/**/*.css",
-                        "/**/*.js"
-                ).permitAll()
-                // allow access login API without require token
-                .antMatchers("/api/**").permitAll()
-                .antMatchers("/**").permitAll()
-                //.antMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
-                // All other reruest must be specify token
-                .anyRequest().authenticated();
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+				// we don't need CSRF because our token is invulnerable
+				.csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				// don't create session
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				// Allow access public resource
+				.antMatchers(HttpMethod.GET, "/", "/upload/**", "/*.html", "/favicon.ico", "/**/*.html", "/**/*.css",
+						"/**/*.js")
+				.permitAll()
+				// allow access login API without require token
+				.antMatchers("/api/**").permitAll()
+				.antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security",
+						"/swagger-ui.html", "/webjars/**", "/swagger-resources/configuration/ui", "/swagge‌​r-ui.html").permitAll()
+				// .antMatchers("/**").permitAll()
+				// .antMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+				// All other reruest must be specify token
+				.anyRequest().authenticated();
 
-        // Custom JWT based security filter
-        httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+		// Custom JWT based security filter
+		httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
 
-        // disable page caching
-        httpSecurity.headers().cacheControl();
-    }
+		// disable page caching
+		httpSecurity.headers().cacheControl();
+	}
 }
